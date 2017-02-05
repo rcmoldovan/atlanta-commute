496ea991fb8d989196d72fe8aacd5d8ea39525bd
Rachel Moldovan <rachel.c.moldovan@gmail.com>
2017-02-05 15:28:09 -0500

    Update atlantacommute.rb

--- atlantacommute.rb
+++ atlantacommute.rb
@@ -1,6 +1,9 @@
 puts "Hello! You are about to play the Atlanta Traffic game. It's the game no one wants to play!"
+user_input = gets
 puts "Good morning! It's Siri, your personal assistant. It's time for your daily commute!"
+user_input = gets
 puts "You are going on an adventure. Through Atlanta. At Rush hour. Every decision you make will randomly effect the amount of time it takes you to get to work."
+user_input = gets
 puts "Are you ready?"
 puts "Answer 'yes' or 'no'."
 user_input = gets.chomp
@@ -22,7 +25,7 @@ elsif user_input == 'no'
 else 
 	puts "There is only do and not-do; there is no in between."
 end
-
+user_input = gets
 # Asks user what year they started driving
 puts "What year did you start driving?"
 # take year and calculate into years by (2016-year)
@@ -33,7 +36,7 @@ age_in_days = age_in_years*365.to_i
 #take age in days and divide it by 86400
 age_in_seconds = age_in_days * 86400.to_i
 #puts age in years, days, and seconds
-puts "You have been driving for " + age_in_years.to_s + " in years,but " + age_in_days.to_s + " in days, and " + age_in_seconds.to_s + " in seconds."
+puts "You have been driving for " + age_in_years.to_s + " years,but that's " + age_in_days.to_s + " in days, and " + age_in_seconds.to_s + " in seconds."
 if age_in_years < 24 
     puts "Which means, according to my calculations, you will not be able to cut people off and still look like a decent person."
 elsif age_in_years > 24
@@ -50,9 +53,11 @@ user_input = gets.chomp
 if user_input == 'leave now'
 	puts "Buckle up! It's going to be one intense ride! Make sure you have your insurance cards!"
 elsif user_input == 'leave later'
-	puts "If you say so...."
+	time = 20 + Random.rand(20)
+	puts "If you say so.... You have lost " + time + " minutes. Time to go!"
 else 
 	puts "What was that? You want to stay home and listen to showtunes all day? Okay. Playing the 'Wicked' Soundtrack now."
+	user_input = gets
 	puts "Do you stay home and listen to 'Popular' twelve times in a row? Please answer 'yes' or 'no'."
 	user_input = gets.chomp
 	if user_input == 'yes'
@@ -66,8 +71,11 @@ end
 
 #get in the car. listen to NPR or to 99X?
 puts "Congratulations on getting all the way to car."
+user_input = gets
 puts "Now that you have started the car and driven to I-75, you come to a slow patch with cars as far as the eye can see."
-puts "Luckily, it is now only 5 minuts into your commute, so you are still in a good mood."
+user_input = gets
+puts "Luckily, it is now only 5 minutes into your commute, so you are still in a good mood."
+user_input = gets
 puts "Radio sounds like a great idea. Please choose 'NPR' or '99X'."
 user_input = gets.chomp
 if user_input == 'NPR'
@@ -90,7 +98,7 @@ else
 	puts "Not a radio person? Fine. Silence is golden."
 	
 end
-
+user_input = gets
 puts "You arrive at a bottleneck. It feels like you are in an infinite loop of traffic."
 #loops-while you are driving, get into a loop of are you frustrated yet
 loopy = false
@@ -106,31 +114,38 @@ end
 
 
 puts "The bottleneck starts to clear."
+user_input = gets
 #drive past someone
 puts "The time is now 8:30am. You have 30 minutes to get to work on time."
+user_input = gets
 puts "You are on the highway and some other cars want to merge. What do you do?"
+user_input = gets
 puts "Please answer 'drive faster' or 'change lanes'. "
 user_input = gets.chomp
 if user_input == 'drive faster'
 	time = 20 + Random.rand(8)
 	puts "You accelerate. You cut the merger off. You have saved " + time.to_s + " minutes."
 elsif user_input == 'change lanes'
-	puts "You swerve into the next left lane. The guy behin you flips you off. You smile in your rearview mirror."
+	puts "You swerve into the next left lane. The guy behind you flips you off. You smile in your rearview mirror."
 else
 end	
-
-puts "You see blue lights blinking behind you."
+user_input = gets
+puts "You see blue lights blinking behind you. You stomach starts to sink."
+user_input = gets
 puts "What do you do?"
 puts "Please answer 'ignore it' or 'pull over'."
 user_input = gets.chomp
+timestuff = 20 + Random.rand(20)
 if user_input == 'ignore it'
-	puts "You are losing " + time.to_s + " minutes the longer you delay this."
+	puts "You are losing " + timestuff.to_s + " minutes the longer you delay this. PULL OVER CRAZY PERSON!"
+	puts "..."
+	puts "Good job pulling over."
 elsif user_input == 'pull over'
-	puts "Good call. You have saved " + time.to_s + "minutes."
+	puts "Good call. You have saved " + timestuff.to_s + "minutes."
 else 
 	puts "Remind me never to ride in the car with you."
 end
-
+user_input = gets
 
 #get pulled over. Police takes your name. you move on with your day
 puts "OFFICER: Hello there. License and registration please. what\'s your name?"
@@ -140,9 +155,10 @@ answer = gets
 mph = 20 + Random.rand(100)
 puts "OFFICER: " + answer.chomp! + " miles per hour?!? It was more like " + mph.to_s + " !"
 user_input = gets
-puts "OFFICER: Okay, " + name + "here is your ticket. You have a lovely day now."
+puts "OFFICER: Okay, " + name + " here is your ticket. You have a lovely day now."
 puts "..."
-puts "SIRI: " + name + " ,you have lost " + time.to_s + " minutes."
+timelost = 20 + Random.rand(10)
+puts "SIRI: " + name + " ,you have lost " + timelost.to_s + " minutes."
 user_input = gets
 puts "You are running late as it is. But there is a Starbucks right ahead."
 puts "You pull in. Might as well, it's not even 9am and your emotions have already had quite the roller coaster."
@@ -151,16 +167,14 @@ puts "You pull in. Might as well, it's not even 9am and your emotions have alrea
 puts "BARISTA: What'll ya have?"
 coffee = gets.chomp
 puts "Order up! Your " + coffee + " will be ready at the bar."
-puts "You wait " + time.to_s + " minutes for your " + coffee + "."
+minutes = 20 + Random.rand(20)
+puts "You wait " + minutes.to_s + " minutes for your " + coffee + "."
 
 puts "...."
 
 puts "You finally pull into the company parking lot. It is 9:05am."
 user_input = gets
 puts "Game over."
-
-
-
 
 
 
